e9bd4e03f2dab7aee44bc96de33315b4a99fc3d1
Iakovos Lampadakis <iakovoslampadakis@gmail.com>
2021-04-02 02:34:28 +0300

    some style changes

--- js/game.js
+++ js/game.js
@@ -1,4 +1,4 @@
-const choices = ['Πέτρα', 'Ψαλίδι', 'Χαρτί'];
+const choices = ['Rock', 'Scissors', 'Paper'];
 let player_choice, computer_choice;
 let player_score = 0;
 let computer_score = 0;
@@ -7,7 +7,7 @@ const info = document.querySelector('#info');
 function play(player_choice){
     computer_choice = Math.floor(Math.random() *3);
     //Eνημερώνεται ο υπολογιστής τι επέλεξε ο καθένας
-    info.innerText = `Επιλέξατε ${choices[player_choice]}, ο υπολογιστής επέλεξε
+    info.innerText = `You chose ${choices[player_choice]}, the computer selected
     ${choices[computer_choice]}`;
     if( player_choice === computer_choice) return;
     if(player_choice - computer_score == -1 || player_choice - computer_choice == 2){
